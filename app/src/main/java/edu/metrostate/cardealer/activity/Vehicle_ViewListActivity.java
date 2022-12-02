12d0c0a0029e6a71f2c6049d57d0c748a15f88fd
DESKTOP-1PA8H8H\ibtha <ibthao10@gmail.com>
2022-12-01 20:28:37 -0600

    Modifications made accordingly to request

--- app/src/main/java/edu/metrostate/cardealer/activity/Vehicle_ViewListActivity.java
+++ app/src/main/java/edu/metrostate/cardealer/activity/Vehicle_ViewListActivity.java
@@ -7,6 +7,7 @@ import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Collections;
 import java.util.List;
 
 import edu.metrostate.cardealer.R;
