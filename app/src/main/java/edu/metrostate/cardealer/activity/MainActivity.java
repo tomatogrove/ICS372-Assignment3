25bdb60dd78f5e7243ec45a64d2c89b250c9263b
tomatogrove <66643214+tomatogrove@users.noreply.github.com>
2022-12-06 19:47:50 -0600

    adding file permission request

--- app/src/main/java/edu/metrostate/cardealer/activity/MainActivity.java
+++ app/src/main/java/edu/metrostate/cardealer/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.metrostate.cardealer.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,30 +17,43 @@ public class MainActivity extends AppCompatActivity {
     private Button vehicle;
     private Button dealer;
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static final String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         vehicle = (Button) findViewById(R.id.all_vehicles);
-        vehicle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFileImport();
-            }
-        });
+        vehicle.setOnClickListener(v -> openFileImport());
 
-        dealer = (Button) findViewById(R.id.specific_dealer);
-        dealer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openDealer();
-            }
-        });
+        dealer = (Button) findViewById(R.id.dealerships);
+        dealer.setOnClickListener(v -> openDealer());
+
+        getFilePermissions();
     }
+
     private void openFileImport() {
         Intent intent = new Intent(this, Vehicle_ImportFileActivity.class);
         startActivity(intent);
+    }
+
+
+    private void getFilePermissions() {
+        int permission = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    MainActivity.this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 
     private void openDealer(){
